b5abb10baf2db29ca7f436a8d245fe9a3871781c
david <david.rodriguez186107@potros.itson.edu.mx>
2022-02-23 22:36:44 -0700

    Le puse una A

--- src/server/Conexion.java
+++ src/server/Conexion.java
@@ -5,10 +5,45 @@
  */
 package server;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  *
  * @author david
  */
 public class Conexion {
     //Aqui hare una conexion
+    static int Puerto = 0; //Se modificara luego, por el momento lo dejare asi
+    InetAddress Direccion = null; //se dara una direccion luego
+    static byte [] BF = new byte[1024];
+    //Puerto y BF me decia que debian estar estaticos, luego puedo cambiarlo si hay problemas con esto
+    public static void Principal(){
+        try {
+            DatagramSocket SocketUDP = new DatagramSocket(Puerto);
+            DatagramPacket Peticion = new DatagramPacket(BF, BF.length);
+        } catch (Exception e) {
+        }
+        
+    }
+    public static int GetPuerto(int puerto){
+        
+        return puerto;
+    }
+    public static InetAddress GetDireccion(InetAddress Direccion){
+        
+        return Direccion;
+    }
+    public static void SetLista(){
+        
+    }
+    public static void SetDatosObjeto(String nombre){
+        //Busqueda del objeto por el nombre y 
+        //luego ya encontrado sera enviada ña informacion de sus datos
+    }
+    public static void SetFaltante(){
+        //el Cliente enviara peticion para datos que falten, desde TODOS hasta UN paquete
+        //este enviara dato a dato
+    }
 }
